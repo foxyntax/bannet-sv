bec891964560c508d9fb68b46c04f11f0b9d0bb8
Legend <milad1995honor@gmail.com>
2021-08-17 16:20:30 +0430

    fix model dir in transaction module

--- Server/modules/Transaction/Models/CoreTransaction.php
+++ Server/modules/Transaction/Models/CoreTransaction.php
@@ -3,9 +3,13 @@
 namespace Modules\Transaction\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\AsCollection;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Modules\Transaction\Database\Factories\CoreTransactionFactory;
 
 class CoreTransaction extends Model
 {
+    use HasFactory;
     /**
      * The table associated with the model.
      *
@@ -72,5 +76,15 @@ class CoreTransaction extends Model
      */
     public function user() {
         return $this->belongsTo('App\User');
+    }
+
+    /**
+     * Create a new factory instance for the model.
+     *
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    protected static function newFactory()
+    {
+        return CoreTransactionFactory::new();
     }
 }
