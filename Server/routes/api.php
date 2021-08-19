eb4410960d80c29d0afd5ef9f12d97fec3533993
Legend <milad1995honor@gmail.com>
2021-08-20 00:35:05 +0430

    update routes

--- Server/routes/api.php
+++ Server/routes/api.php
@@ -17,68 +17,78 @@ use Illuminate\Support\Facades\Route;
 // Route::middleware('auth:sanctum')
 
 // Tested all
-Route::middleware('auth:sanctum')->namespace('User')->prefix('user')->group(function() {
-    Route::prefix('contract')->group(function() {
-        Route::post('/token/generate', 'Contract@generate_withdrawal_req_token');
-        Route::post('/shipment/approve', 'Contract@send_proven_shipment_receipt');
-        Route::post('/withdrawal/pending-balance', 'Contract@withdrawal_pending_balance');
-        Route::post('/cancel', 'Contract@cancel_contract');
-        Route::post('/review', 'Contract@review_user');
-    });
+Route::namespace('User')->prefix('user')->group(function() {
 
-    Route::prefix('membership')->group(function() {
-        Route::get('/fetch', 'Profile@fetch_available_memberships');
-        Route::get('/check/{user_id}', 'Membership@is_memebrship_expired');
-        Route::patch('/buy/{user_id}/{membership_id}', 'Membership@buy_membership_from_wallet');
-    });
-
-    Route::prefix('profile')->group(function() {
-        Route::get('/fetch/{user_id}', 'Profile@fetch_user_data');
-        Route::patch('/update/{user_id}/{mode}', 'Profile@update_user_data');
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::prefix('contract')->group(function() {
+            Route::post('/create', 'Contract@create');
+            Route::post('/token/generate', 'Contract@generate_withdrawal_req_token');
+            Route::post('/shipment/approve', 'Contract@send_proven_shipment_receipt');
+            Route::post('/withdrawal/pending-balance', 'Contract@withdrawal_pending_balance');
+            Route::post('/cancel', 'Contract@cancel');
+            Route::post('/review', 'Contract@review_user');
+        });
+    
+        Route::prefix('membership')->group(function() {
+            Route::get('/fetch', 'Profile@fetch_available_memberships');
+            Route::get('/check/{user_id}', 'Membership@is_memebrship_expired');
+            Route::patch('/buy/{user_id}/{membership_id}', 'Membership@buy_membership_from_wallet');
+        });
+    
+        Route::prefix('profile')->group(function() {
+            Route::get('/fetch/{user_id}', 'Profile@fetch_user_data');
+            Route::patch('/update/{user_id}/{mode}', 'Profile@update_user_data');
+        });
     });
     
     Route::prefix('store')->group(function() {
         Route::prefix('fetch')->group(function () {
-            Route::get('/product/{offset}/{limit}/{get_filters}', 'Products@render_product_page');
+            Route::get('/product/{offset}/{limit}/{get_filters?}', 'Products@render_product_page');
             Route::get('/detail/{product_id}/{user_id}', 'ProductDetail@render_product_detail');
         });
     });
 });
 
 Route::middleware('auth:sanctum')->namespace('Admin')->prefix('cms')->group(function() {
+    // All tested
     Route::prefix('contract')->group(function() {
         Route::prefix('shipment')->group(function () {
-            Route::post('/approve', 'Contract@approve_shipment');
-            Route::post('/disapprove', 'Contract@disapprove_of_receipt');
+            Route::patch('/approve', 'Contract@approve_shipment');
+            Route::patch('/disapprove', 'Contract@disapprove_of_receipt');
         });
 
         Route::prefix('fetch')->group(function () {
-            Route::get('/{status}/{offset}/{limit}/{searched}', 'Contract@fetch_contracts');
-            Route::get('/detail/{contract_id}', 'Contract@fetch_contract_detail');
+            Route::get('/{status}/{offset}/{limit}/{searched?}', 'Contract@fetch');
+            Route::get('/detail/{contract_id}', 'Contract@fetch_detail');
         });
 
-        Route::post('/expire', 'Contract@expire_contracts');
+        Route::patch('/expire', 'Contract@expire');
     });
 
+    // All tested
     Route::prefix('membership')->group(function() {
-        Route::get('/fetch/{status}/{offset}/{limit}/{searched}', 'Membership@fetch_memberships');
-        Route::patch('/update/{membership_id}', 'Membership@update_membership');
-        Route::delete('/delete/{membership_id}', 'Membership@delete_memebrship');
+        Route::get('/fetch/{status}/{offset}/{limit}/{searched?}', 'Membership@fetch');
+        Route::post('/create', 'Membership@create');
+        Route::patch('/update/{membership_id}', 'Membership@update');
+        Route::delete('/delete/{membership_id}', 'Membership@delete');
     });
 
+    // All tested
     Route::prefix('profile')->group(function() {
-        Route::get('/fetch/{is_admin}/{offset}/{limit}/{searched}', 'Profile@fetch_users');
+        Route::get('/fetch/{is_admin}/{offset}/{limit}/{searched?}', 'Profile@fetch');
         Route::patch('/identity/{user_id}', 'Profile@identify_profile');
     });
 
     Route::prefix('store')->group(function() {
+        // All tested
         Route::prefix('fetch')->group(function () {
-            Route::get('/{status}/{offset}/{limit}/{searched}', 'Products@fetch_products');
-            Route::get('/detail/{product_id}', 'Contract@fetch_contract_detail');
+            Route::get('/{type}/{offset}/{limit}/{searched?}', 'Products@fetch');
+            Route::get('/detail/{product_id}', 'Products@fetch_detail');
         });
 
         Route::post('/create', 'Products@create_product');
         Route::patch('/update/{product_id}', 'Products@update_product');
+        // All tested
         Route::delete('/delete/{product_id}', 'Products@delete_product');
     });
 });
