62a723b48c48351920b35e7adccecce17b089f70
Legend <milad1995honor@gmail.com>
2022-01-16 18:34:07 +0330

    fix kavengegar method issues

--- Server/app/Traits/contract/ContractNotice.php
+++ Server/app/Traits/contract/ContractNotice.php
@@ -17,13 +17,16 @@ trait ContractNotice {
     */
     protected function notice_withdrawal() {
         try {
+            // Get product's name
+            $product = CoreProduct::select('features')->where('id', $this->contract->meta['product_id'])->first();
+
             // Send SMS to seller
             $seller = User::select('tell')->where('id', $this->contract->user_id)->first();
-            KavenegarApi::VerifyLookup($seller->tell, $this->contract->meta['cost'], '', '', 'WithdrawalPendingBalanceForSeller', 'sms');
+            KavenegarApi::VerifyLookup($seller->tell, $this->contract->meta['cost'], $product->features['name'], '', 'WithdrawalPendingBalanceForSeller', 'sms');
 
             // Send SMS to Customer
             $customer = User::select('tell')->where('id', $this->contract->user_id)->first();
-            KavenegarApi::VerifyLookup($customer->tell, $this->contract->meta['cost'], '', '', 'WithdrawalPendingBalanceForCustomer', 'sms');
+            KavenegarApi::VerifyLookup($customer->tell, $this->contract->meta['cost'], $product->features['name'], '', 'WithdrawalPendingBalanceForCustomer', 'sms');
             
         } catch(ApiException $e){
             return response()->json($e->errorMessage(), 412);
@@ -42,7 +45,7 @@ trait ContractNotice {
         try {
             // Send SMS to seller
             foreach ($users as $user) {
-                KavenegarApi::VerifyLookup($user->tell, $user->full_name, $user->expired_at, '', 'WithdrawalPendingBalanceForSeller', 'sms');
+                KavenegarApi::VerifyLookup($user->tell, $user->full_name, $user->expired_at, $user->product_features['name'], 'ExpireAllAds', 'sms');
             }
             
         } catch(ApiException $e){
@@ -53,7 +56,7 @@ trait ContractNotice {
     }
 
     /** 
-     ** Take a notice customer sends a withdrawal token to seller by SMS
+     ** Take a notice for sending a withdrawal token to seller by SMS
      * 
      * @return void/Object
     */
@@ -62,8 +65,8 @@ trait ContractNotice {
             // Send SMS to seller
             $seller = User::select('tell')->where('id', $this->contract->user_id)->first();
             $customer = User::select('full_name')->where('id', $this->contract->meta['customer_id'])->first();
-            $product = CoreProduct::select('design_name')->where('id', $this->contract->meta['product_id'])->first();
-            KavenegarApi::VerifyLookup($seller->tell, $this->contract->meta['token'], $customer->full_name, $product->design_name, 'GetWithdrawalTokenForSeller', 'sms');
+            $product = CoreProduct::select('features')->where('id', $this->contract->meta['product_id'])->first();
+            KavenegarApi::VerifyLookup($seller->tell, $this->contract->meta['token'], $customer->full_name, $product->features['name'], 'GetWithdrawalTokenForSeller', 'sms');
             
         } catch(ApiException $e){
             return response()->json($e->errorMessage(), 412);
@@ -81,7 +84,8 @@ trait ContractNotice {
         try {
             // Send SMS to Customer
             $customer = User::select('tell')->where('id', $this->contract->user_id)->first();
-            KavenegarApi::VerifyLookup($customer->tell, $this->contract->meta['cost'], '', '', 'CancelContractForCustomer', 'sms');
+            $product = CoreProduct::select('features')->where('id', $this->contract->meta['product_id'])->first();
+            KavenegarApi::VerifyLookup($customer->tell, $this->contract->meta['cost'], $product->features['name'], '', 'CancelContractForCustomer', 'sms');
             
         } catch(ApiException $e){
             return response()->json($e->errorMessage(), 412);
